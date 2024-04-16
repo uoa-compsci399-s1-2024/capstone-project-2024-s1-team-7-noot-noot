ba0bc5b3c8b9ebc0c0ccfbbcb715ea08b2d207fd
Hamzah Patel <hamzahpatel403@gmail.com>
2024-04-16 23:35:31 +1200

    Added Child button and reformatted entire page

--- SightSaver/app/(tabs)/profile.js
+++ SightSaver/app/(tabs)/profile.js
@@ -1,172 +1,161 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text, View, Switch, Image } from 'react-native';
+import { StyleSheet, Text, View, Switch, TouchableOpacity } from 'react-native';
 import { EventRegister } from 'react-native-event-listeners';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import themeContext from '@/constants/themeContext';
-import { StatusBar } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function ProfileScreen() {
     const [darkMode, setDarkMode] = useState(false);
-    const theme = useContext(themeContext);
-    const [darkModeText, changeText] = useState("Dark Mode")
-    
+    const [activeButton, setActiveButton] = useState(2); // Changed state for active button
 
+    // Access theme context
+    const theme = useContext(themeContext);
+
+    // Initialize dark mode from AsyncStorage
     useEffect(() => {
-        (async () => {
+        const initializeDarkMode = async () => {
             const savedDarkMode = await DarkModeManager.getDarkMode();
             setDarkMode(savedDarkMode);
-        })();
+        };
+        initializeDarkMode();
     }, []);
 
+    // Handle toggling dark mode
     const toggleDarkMode = async (value) => {
         setDarkMode(value);
         EventRegister.emit('ChangeTheme', value);
         await DarkModeManager.setDarkMode(value);
-        changeText("Light Mode")
     };
-    return (
-        <View style={[styles.container, {backgroundColor:theme.backgroundColor}]}>
-          <View style={styles.header}>
-            <View style={styles.headerContent}>
-                {/* Parent Profile  */}
-              <Image
-                style={styles.avatar}
-                source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }}
-              />
-              <Text style={styles.name}>John Doe </Text>
-              <Text style={styles.userInfo}>jhonnydoe@mail.com </Text>
-              <Text style={styles.userInfo}>Florida </Text>
-            </View>
-          </View>
-                {/* Profile Options  */}
-          <View style={styles.body}>
-            <View style={styles.item}>
-              <View style={styles.iconContent}>
-                <Image
-                  style={styles.icon}
-                  source={{ uri: 'https://img.icons8.com/color/70/000000/cottage.png' }}
-                />
-              </View>
-              <View style={styles.infoContent}>
-                <Text style={styles.info}>Home</Text>
-              </View>
-            </View>
-    
-            <View style={styles.item}>
-              <View style={styles.iconContent}>
-                <Image
-                  style={styles.icon}
-                  source={{ uri: 'https://img.icons8.com/color/70/000000/administrator-male.png' }}
-                />
-              </View>
-              <View style={styles.infoContent}>
-                <Text style={styles.info}>Settings</Text>
-              </View>
-            </View>
-    
-            <View style={styles.item}>
-              <View style={styles.iconContent}>
-                <Image
-                  style={styles.icon}
-                  source={{ uri: 'https://img.icons8.com/color/70/000000/filled-like.png' }}
-                />
-              </View>
-              <View style={styles.infoContent}>
-                <Text style={styles.info}>News</Text>
-              </View>
-            </View>
-    
-            <View style={styles.item}>
-              <View style={styles.iconContent}>
-                <Image
-                  style={styles.icon}
-                  source={{ uri: 'https://img.icons8.com/color/70/000000/facebook-like.png' }}
-                />
-              </View>
-              <View style={styles.infoContent}>
-                <Text style={styles.info}>Shop</Text>
-              </View>
-            </View>
-            <View style={[styles.container, {backgroundColor:theme.backgroundColor}]}>
-             <StatusBar barStyle={theme.barStyle}/>
-             <Text style={[styles.title, {color:theme.color}]}>Switch to {darkModeText}</Text>
-             <Switch
-                value={darkMode}
-                onValueChange={toggleDarkMode}
-            />
-          </View>
-        </View>
-        </View>
-      )
-    }
-   
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    header: {
-        // backgroundColor: '#DCDCDC',
-      },
-      headerContent: {
-        padding: 30,
-        alignItems: 'center',
-      },
-      avatar: {
-        width: 130,
-        height: 130,
-        borderRadius: 63,
-        borderWidth: 4,
-        borderColor: 'white',
-        marginBottom: 10,
-      },
-      name: {
-        fontSize: 22,
-        color: '#000000',
-        fontWeight: '600',
-      },
-      userInfo: {
-        fontSize: 16,
-        color: '#778899',
-        fontWeight: '600',
-      },
-      body: {
-        // backgroundColor: '#778899',
-        height: 500,
-        alignItems: 'center',
-      },
-      item: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      infoContent: {
-        flex: 1,
-        alignItems: 'flex-start',
-        paddingLeft: 5,
-      },
-      iconContent: {
-        flex: 1,
-        alignItems: 'flex-end',
-        paddingRight: 5,
-      },
-      icon: {
-        width: 30,
-        height: 30,
-        marginTop: 20,
-      },
-      info: {
-        fontSize: 18,
-        marginTop: 20,
-        color: '#FFFFFF',
-      },
-});
+    // Handle button press
+    const handlePress = (index) => {
+        setActiveButton(index);
+    };
+
+    // Get the styles based on the current theme
+    const themedStyles = getThemedStyles(theme);
+
+    return (
+        <View style={[themedStyles.container]}>
+            {/* Parent Profile */}
+            <View style={themedStyles.header}>
+                <View style={themedStyles.headerContent}>
+                    <MaterialCommunityIcons name="face-man-profile" size={50} color="black" />
+                    
+                    <View style={themedStyles.headerText}>
+                        <Text style={themedStyles.name}>Jane Doe</Text>
+                        <Text style={themedStyles.userInfo}>info@company.com</Text>
+                    </View>
+                </View>
+            </View>
+
+            {/* Options */}
+            <View style={themedStyles.body}>
+                {/* Dark Mode toggle */}
+                <View style={themedStyles.darkModeToggle}>
+                    <Text style={themedStyles.title}>Dark Mode</Text>
+                    <Switch
+                        value={darkMode}
+                        onValueChange={toggleDarkMode}
+                    />
+                </View>
+            </View>
+
+            {/* Children buttons */}
+            <View>
+              {/* Child 1 Button */}
+              <TouchableOpacity
+                  style={themedStyles.button}
+                  onPress={() => handlePress(1)} // Pass index 1
+              >
+                  <Text>Child 1</Text>
+                  <View
+                      style={[
+                          themedStyles.circle,
+                          activeButton === 1 && themedStyles.circlePressed, // Apply style when button 1 is active
+                      ]}
+                  />
+              </TouchableOpacity>
+
+              {/* Child 2 Button */}
+              <TouchableOpacity
+                  style={themedStyles.button}
+                  onPress={() => handlePress(2)} // Pass index 2
+              >
+                  <Text>Child 2</Text>
+                  <View
+                      style={[
+                          themedStyles.circle,
+                          activeButton === 2 && themedStyles.circlePressed, // Apply style when button 2 is active
+                      ]}
+                  />
+              </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+// Function that returns styles based on the theme
+function getThemedStyles(theme) {
+    return StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: theme.backgroundColor,
+        },
+        header: {
+            display: 'flex',
+            justifyContent: 'space-between',
+        },
+        headerContent: {
+            padding: 30,
+            flexDirection: 'row',
+            // borderBottomWidth: 1,
+
+        },
+        headerText: {
+            marginLeft: 10,
+        },
+        name: {
+            fontSize: 22,
+            fontWeight: '600',
+            color: theme.color,
+        },
+        userInfo: {
+            fontSize: 16,
+            fontWeight: '600',
+            color: theme.color,
+        },
+        body: {
+          alignItems: 'center',
+        },
+        darkModeToggle: {
+            marginTop: 20,
+        },
+        title: {
+          fontSize: 18,
+          marginBottom: 10,
+          color: theme.color,
+        },
+        button: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          padding: 10,
+          borderWidth: 1,
+          borderColor: 'black',
+          borderRadius: 5,
+        },
+        circle: {
+            width: 20,
+            height: 20,
+            borderRadius: 10,
+            backgroundColor: theme.color,
+        },
+        circlePressed: {
+            backgroundColor: 'green',
+        },
+    });
+}
 
 class DarkModeManager {
     static DARK_MODE_KEY = 'darkMode';
