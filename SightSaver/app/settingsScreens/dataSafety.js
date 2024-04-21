3dd7c2dbd67de8cd89f878e32f7a23a296b80688
Hamzah Patel <hamzahpatel403@gmail.com>
2024-04-21 19:46:40 +1200

    Update dataSafety.js

--- SightSaver/app/settingsScreens/dataSafety.js
+++ SightSaver/app/settingsScreens/dataSafety.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import CustomButton from '../../components/CustomButton';
 
 
 export default function DataSafety() {
