951f53f7e18c88851be139accad31f38020e2702
Ch3ssman <jbau2003@gmail.com>
2024-04-21 21:30:51 +1200

    Create user model and update test controller

---
+++ Backend_API - NootNoot/src/main/java/NootNoot/SightSaver/model/User.java
@@ -0,0 +1,57 @@
+package NootNoot.SightSaver.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private boolean parent;
+
+    public User() {}
+
+    public User(String username, String password, String email, boolean parent) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.parent = parent;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public boolean getParent() {
+        return this.parent;
+    }
+
+    public void setUsername(String newUsername) {
+        this.username = newUsername;
+    }
+
+    public void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    public void setParent(boolean value) {
+        this.parent = value;
+    }
+
+}
