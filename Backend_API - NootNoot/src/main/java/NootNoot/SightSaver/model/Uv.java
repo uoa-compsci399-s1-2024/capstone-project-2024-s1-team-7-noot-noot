9a46e8117be39247058db66b4f2651d484c155fc
jdiegoms <jsan829@aucklanduni.ac.nz>
2024-04-22 17:42:25 +1200

    Created Uv class

---
+++ Backend_API - NootNoot/src/main/java/NootNoot/SightSaver/model/Uv.java
@@ -0,0 +1,35 @@
+package NootNoot.SightSaver.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+public class Uv{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private float uv_value;
+    private LocalDateTime date_time;
+
+
+    public Uv() {
+    }
+
+
+
+    public Uv(float uv_value, String date_time) {
+        this.date_time = LocalDateTime.parse(date_time, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.uv_value = uv_value;
+
+    }
+
+
+    public float getLux_value(){return this.uv_value;}
+    public LocalDateTime getDate_time(){return this.date_time;}
+
+    public void setLux_value(float lux_value){this.uv_value = lux_value;}
+    public void setDate_time(String date_time){this.date_time = LocalDateTime.parse(date_time, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));}
+
+}
