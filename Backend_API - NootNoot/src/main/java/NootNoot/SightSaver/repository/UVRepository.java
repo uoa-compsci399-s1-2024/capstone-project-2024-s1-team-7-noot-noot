e360783bdcca4ca5dfe60961b60f446384c84deb
Ju-an <84208158+Ch3ssman@users.noreply.github.com>
2024-04-22 20:29:08 +1200

    Update UVRepository.java

--- Backend_API - NootNoot/src/main/java/NootNoot/SightSaver/repository/UVRepository.java
+++ Backend_API - NootNoot/src/main/java/NootNoot/SightSaver/repository/UVRepository.java
@@ -3,9 +3,9 @@ package NootNoot.SightSaver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import NootNoot.SightSaver.model.UV;
+import NootNoot.SightSaver.model.Uv;
 
 @Repository
-public interface UVRepository extends JpaRepository<UV, Long> {
+public interface UVRepository extends JpaRepository<Uv, Long> {
     // Add additional queries as necessary 
 }
