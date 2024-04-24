c9ba685e89955dfa79746e7e77b59fb28dd27719
jdiegoms <jsan829@aucklanduni.ac.nz>
2024-04-24 19:01:00 +1200

    Immplemented Feedback Controller and updated Feedback service

---
+++ Backend_API - NootNoot/src/main/java/NootNoot/SightSaver/controller/FeedbackController.java
@@ -0,0 +1,39 @@
+package NootNoot.SightSaver.controller;
+
+import NootNoot.SightSaver.model.Feedback;
+import NootNoot.SightSaver.service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/feedback")
+public class FeedbackController {
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @GetMapping
+    public List<Feedback> getAll() {
+        return feedbackService.getAllFeedback();
+    }
+
+    @GetMapping("/{id}")
+    public Feedback getFeedbackById(@PathVariable Long id) {
+        return feedbackService.getFeedbackById(id);
+    }
+
+    @PostMapping
+    public Feedback addFeedback(@RequestBody Feedback feedback) {
+        return feedbackService.saveFeedback(feedback);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFeedback(@PathVariable Long id) {
+        feedbackService.deleteFeedbackbyID(id);
+    }
+
+}
