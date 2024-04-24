4e1d88d91fbe48d43c994a10ad9ccd1d21b547e7
jdiegoms <118315083+jdiegoms@users.noreply.github.com>
2024-04-24 18:50:56 +1200

    Update Database_queries.sql

--- Documentation/Back-end Designs/Database_queries.sql
+++ Documentation/Back-end Designs/Database_queries.sql
@@ -3,119 +3,109 @@ CREATE DATABASE sightsaver_db;
 USE sightsaver_db;
 
 /* Create the users table */
-CREATE TABLE users (
-	user_id int PRIMARY KEY,
+CREATE TABLE [user] (
+	id int PRIMARY KEY IDENTITY(1,1),
+	email varchar(255) UNIQUE NOT NULL,
+	parent bit NOT NULL,
 	username varchar(255) UNIQUE NOT NULL,
-    password varchar(255) NOT NULL,
-    email varchar(255) UNIQUE NOT NULL,
-    parent boolean NOT NULL
+    password varchar(255) NOT NULL
 );
 
 /* Add test data into user table */
-INSERT INTO users VALUES ( 1, 'Admin', 'Password', 'admin@gmail.com', true);
+INSERT INTO [user] VALUES ('admin@gmail.com', 1, 'Admin', 'Password');
 
 /* Query user table */
-SELECT * FROM users;
+SELECT * FROM [user];
 
 /* Create the feedback table */
-CREATE TABLE feedback (
-	feedback_id int PRIMARY KEY,
+CREATE TABLE feedback(
+	id int PRIMARY KEY IDENTITY(1,1),
     comment varchar(255) NOT NULL
 );
 
 /* Add test data into user table */
-INSERT INTO feedback VALUES ( 1, 'This is a test of the feedback comment');
+INSERT INTO feedback VALUES ('This is a test of the feedback comment');
 
 /* Query feedback table */
 SELECT * FROM feedback;
 
 /* Create the parents table */
-CREATE TABLE parents (
-	parent_id int PRIMARY KEY, 
-    parents_name varchar(255), 
-    uv_goal float DEFAULT 2,
-    number_of_children int DEFAULT 0,
+CREATE TABLE parent (
+	id int PRIMARY KEY IDENTITY(1,1), 
     feedback_id int,
-    user_id int NOT NULL,
-    FOREIGN KEY (feedback_id) REFERENCES feedback(feedback_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    name varchar(255), 
+    number_of_children int DEFAULT 0,
+    uv_goal float DEFAULT 2
 );
 
 /* Add test data into parent table */
-INSERT INTO parents  VALUES ( 1, 'parent_test_name', 5.0, 2, 1, 1 ); 
+INSERT INTO parent VALUES (1, 'parent_test_name', 2, 5 ); 
 
 /* Query parents table */
-SELECT * FROM parents;
+SELECT * FROM parent;
 
 /* Create the uv table */
 CREATE TABLE uv (
-    uv_id int PRIMARY KEY, 
-    uv_value float,
-    uv_date datetime
+    id int PRIMARY KEY IDENTITY(1,1), 
+    date_time datetime,
+	uv_value float
 );
 
 /* Add test data into float table */
-INSERT INTO uv VALUES ( 1, 4.8, '2024-04-21 13:58:22');
+INSERT INTO uv VALUES ( '2024-04-21 13:58:22', 4.8);
 
 /* Query uv table */
 SELECT * FROM uv;
 
 /* Create the lux table */
 CREATE TABLE lux (
-    lux_id int PRIMARY KEY, 
-    lux_value float,
-    lux_date datetime
+    id int PRIMARY KEY IDENTITY(1,1), 
+    date_time datetime,
+    lux_value float
 );
 
 /* Add test data into lux table */
-INSERT INTO lux VALUES ( 1, 30264.0, '2024-04-21 13:58:22');
+INSERT INTO lux VALUES ('2024-04-21 13:58:22', 30264.0);
 
 /* Query lux table */
 SELECT * FROM lux;
 
 /* Create the sensors table */
-CREATE TABLE sensors (
-    sensor_id int PRIMARY KEY, 
+CREATE TABLE sensor (
+    id int PRIMARY KEY IDENTITY(1,1), 
+	lux_id int,
     user_id int NOT NULL,
-    lux_id int,
-    uv_id int ,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (lux_id) REFERENCES lux(lux_id),
-    FOREIGN KEY (uv_id) REFERENCES uv(uv_id)
+    uv_id int
 );
 
 /* Add test data into sensors table */
-INSERT INTO sensors VALUES ( 1, 1, 1, 1);
+INSERT INTO sensor VALUES (1, 1, 1);
 
 /* Query sensors table */
-SELECT * FROM sensors;
+SELECT * FROM sensor;
 
 /* Create the child table */
 CREATE TABLE child (
-	child_id int PRIMARY KEY, 
-    childs_name varchar(255), 
-    sensor_id int NOT NULL, 
+	id int PRIMARY KEY IDENTITY(1,1), 
+    name varchar(255), 
 	parent_id int NOT NULL,
-    FOREIGN KEY (sensor_id) REFERENCES sensors(sensor_id),
-    FOREIGN KEY (parent_id) REFERENCES parents(parent_id)
+    sensor_id int NOT NULL
 );
 
 /* Add test data into child table */
-INSERT INTO child VALUES ( 1, 'child_test_name', 1, 1 );
+INSERT INTO child VALUES ('child_test_name', 1, 1 );
 
 /* Query child table */
 SELECT * FROM child;
 
 /* Create the researchers table */
-CREATE TABLE researchers (
-    researcher_id int PRIMARY KEY, 
-    researcher_name varchar(255) NOT NULL, 
-    user_id int NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE researcher (
+    id int PRIMARY KEY IDENTITY(1,1), 
+    name varchar(255) NOT NULL
 );
 
 /* Add test data into researcher table */
-INSERT INTO researchers  VALUES ( 1, 'researcher_test_name', 1 );
+INSERT INTO researcher  VALUES ('researcher_test_name');
 
 /* Query researchers table */
-SELECT * FROM researchers;
+SELECT * FROM researcher;
