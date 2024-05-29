8179bf08af8a1559dcf050239f159fddbcba0006
Euan Rix <109153013+euanrix@users.noreply.github.com>
2024-05-29 22:48:12 +1200

    Delete launcher.sh

--- launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#launcher.sh
-#Navigate to home directory, then this directory to execute python script
-
-cd /
-cd home/teamnootnoot/capstone-project-2024-s1-team-7-noot-noot/Hardware
-sudo python3 luxv2.py
-sudo python3 BLEServer.py
-cd/
