c1e05f26c1c8ae3d9e52a3f7130a94f77ee00136
Euan Rix <euanrix@gmail.com>
2024-05-31 17:06:30 +1200

    Fixed Package.Json bug

--- SightSaver/app/(entry)/settingsScreens/children.js
+++ SightSaver/app/(entry)/settingsScreens/children.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useColorScheme } from '../../../components/useColorScheme';
 import Colors from '../../../constants/Colors';
-import { Slider } from '@rneui/themed';
 import * as SecureStore from 'expo-secure-store';
 
 export default function NotificationSettings() {
@@ -10,67 +9,15 @@ export default function NotificationSettings() {
     const [dailyGoal, setDailyGoal] = useState(2);
     const [initialGoal, setInitialGoal] = useState(2);
 
-    const saveDailyGoal = async () => {
-        try {
-            await SecureStore.setItemAsync('dailyGoal', dailyGoal.toString());
-        } catch (error) {
-            console.error('Error saving daily goal:', error);
-        }
-    };
-
-    useEffect(() => {
-        const loadDailyGoal = async () => {
-            try {
-                const goal = await SecureStore.getItemAsync('dailyGoal');
-                if (goal) {
-                    const parsedGoal = parseInt(goal, 10);
-                    setInitialGoal(parsedGoal);
-                    setDailyGoal(parsedGoal);
-                }
-            } catch (error) {
-                console.error('Error loading daily goal:', error);
-            }
-        };
-        loadDailyGoal();
-    }, []);
-
-    const incrementGoal = () => {
-        setDailyGoal((prevGoal) => Math.min(prevGoal + 1, 12));
-    };
-
-    const decrementGoal = () => {
-        setDailyGoal((prevGoal) => Math.max(prevGoal - 1, 1));
-    };
-
     return (
         <View style={[styles.container, { backgroundColor: Colors[colorScheme ?? 'light'].background }]}>
             <View style={styles.textArea}>
                 <Text style={[styles.title, { color: Colors[colorScheme ?? 'light'].text }]}>Daily Goal:</Text>
                 <Text style={[styles.text, { color: Colors[colorScheme ?? 'light'].text, marginLeft: '5%' }]}>{dailyGoal} Hours</Text>
             </View>
-            <View style={styles.section}>
-                <Slider
-                    animateTransitions
-                    animationType="timing"
-                    maximumTrackTintColor="#ccc"
-                    maximumValue={24}
-                    minimumTrackTintColor="#222"
-                    minimumValue={0}
-                    onValueChange={value => setDailyGoal(value)}
-                    orientation="horizontal"
-                    step={1}
-                    style={styles.slider}
-                    thumbStyle={{ height: 20, width: 20 }}
-                    thumbTintColor="#0c0"
-                    thumbTouchSize={{ width: 40, height: 40 }}
-                    trackStyle={{ height: 10, borderRadius: 20 }}
-                    value={initialGoal}
-                />
-            </View>
             <View>
                 <TouchableOpacity
                     style={styles.saveButton}
-                    onPress={saveDailyGoal}
                 >
                     <Text style={styles.saveButtonText}>Save Data</Text>
                 </TouchableOpacity>
