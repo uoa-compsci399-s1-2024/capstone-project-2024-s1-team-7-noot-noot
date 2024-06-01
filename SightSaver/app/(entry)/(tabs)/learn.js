ce072886b97edb97647f76da94c6e91456656f87
solosophia <sophiablacknz@gmail.com>
2024-06-02 00:02:27 +1200

    add text and images to learn page

--- SightSaver/app/(entry)/(tabs)/learn.js
+++ SightSaver/app/(entry)/(tabs)/learn.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, Image, useWindowDimensions} from 'react-native';
 import { Text, View } from '../../../components/Themed';
 import Colors from '../../../constants/Colors';
 import { useColorScheme } from '../../../components/useColorScheme';
@@ -7,6 +7,7 @@ import { StatusBar } from 'react-native';
 
 export default function LearnScreen() {
   const colorScheme = useColorScheme();
+  const { height } = useWindowDimensions();
   return (
     <ScrollView>
     <View style={styles.container}>
@@ -20,28 +21,46 @@ export default function LearnScreen() {
       </Text>
 
           <Text style={styles.centerText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
-              Ipsum dolor sit amet consectetur adipiscing elit ut. Risus ultricies tristique nulla aliquet. {"\n"}
+              Myopia, also known as nearsightedness, is a common condition that usually starts in childhood. 
+              While myopia itself isn't new, the alarming rate at which it's increasing is. 
+              {"\n"}A 2016 Sydney Myopia Study revealed a concerning trend: in 2000, only 22.9% of the global population was myopic. 
+              By 2050, that number is expected to skyrocket to nearly 50%. {"\n"}
+          </Text>
+        
+            <Image source={require('../../../assets/images/myopiaComp.jpg')}  style={[styles.logo, {height:height*0.3, width:height*0.435,}]}/>
+        
+            <Text style={[styles.subTitle, {color: Colors[colorScheme ?? 'light'].subTitle}]}>
+            Sight Saver goals{"\n"}
           </Text>
 
-      <Text style={[styles.subTitle, {color: Colors[colorScheme ?? 'light'].subTitle}]}>
-            Sight Saver goals{"\n"}
-      </Text>
-
           <Text style={styles.centerText} >
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. {"\n"}
+          Studies have shown that the chances of kids becoming myopic is reduced by around one-third if time spent outdoors is 
+          increased from 0 to 5 hours per week to 14 or more hours per week.”  {"\n"}
+          Another suggested that with an increase in time spent outdoors from 1 hour to 3 hours per day, 
+          the risk of myopia could be reduced by 50%. Not only that, but spending more time outdoors also has other positive 
+          outcomes for children and adolescents, such as obesity prevention and promoting a healthier lifestyle.” 
+          {"\n"}{"\n"}
+        Despite the many benefits of myopia management through sunlight, there are currently no full-scale solutions involving 
+        light-sensing devices and applications to help parents and children preserve their vision. {"\n"}
+        Our solution aims to create an app to help parents keep track of their child’s time outside and automate this process 
+        through a purpose-built wearable light-sensing device. Not only this but we also plan to use this device to record
+         accurate and ethical data for use in further studies into myopia by optometrist researchers at the 
+         University of Auckland.
+ {"\n"}
           </Text>
 
       <Text style={[styles.subTitle, {color: Colors[colorScheme ?? 'light'].subTitle}]}>
         Myopia Treatment options{"\n"}
       </Text>
           <Text style={styles.centerText} > 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. {"\n"}
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                {"\n"}
-                Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Non nisi est sit amet. Non blandit massa enim nec. 
-                Cras tincidunt lobortis feugiat vivamus at augue eget. Ornare lectus sit amet est.{"\n"}
+          There are several effective ways to help prevent and reduce myopia, particularly in children, 
+          such as 1% atropine eye drops and multifocal contact lenses. 
+          These measures, however, require the child to visit an optometrist and be at risk of, or already in the process of developing myopia. 
+                {"\n"}{"\n"}
+                Fortunately, there is evidence that the management of environmental factors such as sunlight is 
+                effective in lowering the risk of developing myopia and slowing its progression.
+                 We also know this can be provided for all children without the same monetary or medical barriers as pharmaceutical measures. 
+                 {"\n"}
           </Text>
       
       <Text style={[styles.subTitle, {color: Colors[colorScheme ?? 'light'].subTitle}]}>
@@ -49,20 +68,24 @@ export default function LearnScreen() {
       </Text>
       
           <Text style={styles.centerText} >
-                Ipsum dolor sit amet consectetur adipiscing elit ut. Risus ultricies tristique nulla aliquet. 
-                Feugiat nibh sed pulvinar proin gravida hendrerit lectus a.
-                Facilisi etiam dignissim diam quis enim lobortis scelerisque. Amet risus nullam eget felis eget nunc. 
-                Ultrices eros in cursus turpis massa tincidunt.
-                {"\n"}{"\n"}
-                Ipsum dolor sit amet consectetur adipiscing elit ut.
-                Ipsum dolor sit amet consectetur adipiscing elit ut. Risus ultricies tristique nulla aliquet. Feugiat nibh sed pulvinar proin gravida hendrerit lectus a.
-                Facilisi etiam dignissim diam quis enim lobortis scelerisque. Amet risus nullam eget felis eget nunc. Ultrices eros in cursus turpis massa tincidunt.
-                Ipsum dolor sit amet consectetur adipiscing elit ut.{"\n"}
+          Why is the prevalence of myopia growing at such a rate? This global rise is not due to genetic factors alone. 
+          Environmental factors are key here; less time spent outdoors has been shown to be a significant risk factor for myopia and its progression. 
+          {"\n"}There is also evidence that increased ‘near work’, such as writing or looking at a device from a short distance, can worsen this risk further.
+           In highly urbanised areas with long schooling hours, we see the number of students with myopia increase significantly, backing this claim. 
+           </Text>
+           <Image source={require('../../../assets/images/myopiaGraph.webp')}  style={[styles.logo, {height:height*0.3, width:height*0.435,marginTop: 10}]}/>
+           <Text style={styles.centerText} >{"\n"}{"\n"}A study on high school students in Beijing found a staggering 80% of the students had myopia. 
+           It is understood that long school hours, high amounts of near work, 
+           and the lack of outdoor activities are the leading causes of these concerningly high statistics. {"\n"}{"\n"}
 
-                Ipsum dolor sit amet consectetur adipiscing elit ut. Risus ultricies tristique nulla aliquet. Feugiat nibh sed pulvinar proin gravida hendrerit lectus a.
-                Facilisi etiam dignissim diam quis enim lobortis scelerisque. Amet risus nullam eget felis eget nunc. Ultrices eros in cursus turpis massa tincidunt.
-                Ipsum dolor sit amet consectetur adipiscing elit ut.{"\n"}
+          It is not only the number of individuals with myopia increasing, however, 
+           but also the severity of myopia itself that is worsening. 
+           It is said that high and pathological myopia is expected to be the “most common cause of irreversible vision impairment and blindness worldwide”.
+            From this, we can see the severity and scale of this issue demand better prevention technologies. 
+        
+{"\n"}
           </Text>
+          
       </View>
     </View>
     </ScrollView>
@@ -76,7 +99,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   textContainer: {
-    width: '80%',
+    width: '90%',
   },
   title: {
     fontSize: 30,
